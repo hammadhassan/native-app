cde7ee57c8ca686ff194e1a701b66a3351a9c839
Hammad Hassan <hammadhassansheikh@gmail.com>
2017-07-30 17:58:53 +0500

    fix home page

--- App.js
+++ App.js
@@ -1,43 +1,43 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-// import Home from "./src/home";
-// import About from "./src/about";
-// import Items from "./src/item";
-
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Home />
-//         <Items />
-//         <About />
-//         <Text>Toolmarts&reg; app in React Native By Hammad Hassan&trade; &copy;</Text>
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-//import React from 'react'
 import { AppRegistry } from 'react-native';
-import { Provider } from 'react-redux';
-import store from './src/store';
-import TabBarNavigation from './src/tabBar/views/TabBarNavigation';
+import Home from "./src/home";
+import About from "./src/about";
+import Items from "./src/item";
 
-class SampleNavigation extends React.Component {
-  render(){
-    return(
-      <Provider store={store}>
-        <TabBarNavigation />
-      </Provider>
-    )
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Home />
+        <Items />
+        <About />
+        <Text>Toolmarts&reg; app in React Native By Hammad Hassan&trade; &copy;</Text>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+//import React from 'react'
+// import { Provider } from 'react-redux';
+// import store from './src/store';
+// import TabBarNavigation from './src/tabBar/views/TabBarNavigation';
+
+// class SampleNavigation extends React.Component {
+//   render(){
+//     return(
+//       <Provider store={store}>
+//         <TabBarNavigation />
+//       </Provider>
+//     )
+//   }
+// }
 AppRegistry.registerComponent('SampleNavigation', () => SampleNavigation)
